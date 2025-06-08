8d181f7e0d2f41023f5a8f01b9408c86079559e1
Itziar Diaz <itziar.diaz-herranz@epitech.eu>
2025-06-08 18:39:16 +0200

    wip: step 2. Guess the number with hints

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import random
 
 secret_number = random.randint(1,100)
 
-print("I'm thinking of a number! Try to guess the number I'm thinking of: ")
-guess = int(input())
-if guess < secret_number:
-    print("Too low! Guess again.")
-elif guess > secret_number:
-    print("Too high! Guess again.")
-else:
-    print("That's it!")
+print("I'm thinking of a number between 1 and 100!")
+guess = None
+while guess != secret_number:
+    guess = int(input("Try to guess the number I'm thinking of: "))
+    if guess < secret_number:
+        print("Too low! Guess again.")
+    elif guess > secret_number:
+        print("Too high! Guess again.")
+    else:
+        print("That's it!")
