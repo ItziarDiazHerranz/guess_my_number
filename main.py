3b41cd3966328ce5b37d5fe50632dd8e9e880a84
Itziar Diaz <itziar.diaz-herranz@epitech.eu>
2025-06-09 10:42:44 +0200

    fix: added flake8 for coding and structure

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import random
 
-play_again = "yes"
-
-while play_again.lower() == "yes":
+def play_game():
     secret_number = random.randint(1, 100)
     print("I'm thinking of a number between 1 and 100!")
+
     guess = None
     attempts = 0
+
     while guess != secret_number:
         try:
             guess = int(input("Try to guess the number I'm thinking of: "))
             attempts += 1
+
             if guess < secret_number:
                 print("Too low! Guess again.")
             elif guess > secret_number:
@@ -18,6 +19,16 @@ while play_again.lower() == "yes":
             else:
                 print(f"That's it! You got it in {attempts} tries!")
         except ValueError:
-            print("Thats not a number! Try again.")
-    play_again = input("Would you like to play again? (yes/no)")
-print("Thanks for playing!")
+            print("That's not a number! Try again.")
+
+def main():
+    play_again = "yes"
+
+    while play_again.lower() == "yes":
+        play_game()
+        play_again = input("Would you like to play again? (yes/no): ")
+    print("Thanks for playing!")
+
+
+if __name__ == "__main__":
+    main()
